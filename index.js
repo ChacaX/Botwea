53af7bb5ba08ae32e5c9a2a5f83188275d0dba6d
MITSUHA-XXX <77813696+ChacaX@users.noreply.github.com>
2021-12-12 02:05:05 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1202,7 +1202,7 @@ teks =`*INFROMATION SYSTEM BOT*
 ❒ ${prefix2}toimg`
 sendButDocument(from, `${teks}`, `\n`, fs.readFileSync(`./lib/odc.jpeg`), {mimetype: Mimetype.pdf, thumbnail:fs.readFileSync(`./lib/odc.jpeg`), filename: `MITSUHA BOT BETA 🦈`}, [{buttonId:`DEVELOPER`,buttonText:{displayText:'DEVELOEPER'},type:1},{buttonId:`SOURCE CODE`,buttonText:{displayText:'SOURCE CODE'},type:1},{buttonId:`HOW TO USE`,buttonText:{displayText:'HOW TO USE'},type:1}])
 break
-
+}
 
 					if (isGroup && isSimi && budy != undefined) {
 						console.log(budy)
