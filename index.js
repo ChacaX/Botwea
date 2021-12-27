71c878953437194b8c310545a8d1d0b92f3b6518
MITSUHA-XXX <77813696+ChacaX@users.noreply.github.com>
2021-12-27 12:54:03 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2023,7 +2023,7 @@ ${rumah} house : ${getHouseUser(sender)}
 
 *Ekonomi*
 ${pabrik} pabrik : ${getPabrikUser(sender)}/${batespv}
-${monumen} monumen : ${getMonumenUser(sender) /${batesmv}
+${monumen} monumen : ${getMonumenUser(sender)}/${batesmv}
 ${hiburan} hiburan : ${getHiburanUser(sender)}/${bateshv}
 🏬 hotel : ${bateshtx}
 💵 money : $${getMoneyUser(sender)}
