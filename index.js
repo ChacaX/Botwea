3c90fbe6659e484460d66319295fcc82f6a3f56e
MITSUHA-XXX <77813696+ChacaX@users.noreply.github.com>
2022-01-16 00:37:17 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -764,12 +764,12 @@ ppimg = await client.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.u
 ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 }
 let buffer = await getBuffer(ppimg)
-client.sendMessage(mdata.id, `Hi @${num.split('@')[0]}, selamat satang di grup ${mdata.subject}\n${mdata.desc}\n📌 This mesagge will be deleted after 5 minutes`, MessageType.text) 
-setTimeout( () => {
-i = mdata.id
-client.deleteMessage(mdata.id, { id: i.message.extendedTextMessage.contextInfo.stanzaId, remoteJid: mdata.id, fromMe: true })
-}, 300000)
+client.sendMessage(mdata.id, `Hi @${num.split('@s.whatsapp.net')[0]} , selamat satang di grup ${mdata.subject}\n\n${mdata.desc}\n\n📌 This mesagge will be deleted after 1 minutes`, MessageType.text) 
 addBadwordId(sender)
+setTimeout( () => {
+i = client.user.jid
+client.deleteMessage(mdata.id, { id: i.message.extendedTextMessage.contextInfo.stanzaId, remoteJid: mdata.id, fromMe: false })
+}, 60000)
 
 } else if (anu.action == 'remove') {
 num = anu.participants[0]
